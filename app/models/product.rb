ab59b79efaeebf199f69599c20fe4bcfcaf202dd
zofiabialla <zofiabialla@gmail.com>
2014-11-26 19:59:58 +0100

    calculate average rating

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,13 @@ class Product < ActiveRecord::Base
   has_many :reviews
   validates_presence_of :price, :title, :description
   validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => true
+
+  def average_rating
+    if self.reviews.size > 0
+   		self.reviews.average(:rating) 
+   	else
+        "there are no ratings yet"
+    end
+  end
+
 end
