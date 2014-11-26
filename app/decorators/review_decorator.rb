1b73e18bc464abc933070482844cd7a5e5946bf0
zofiabialla <zofiabialla@gmail.com>
2014-11-26 20:39:21 +0100

    display reviews author full name

--- app/decorators/review_decorator.rb
+++ app/decorators/review_decorator.rb
@@ -2,6 +2,11 @@ class ReviewDecorator < Draper::Decorator
   delegate_all
 
   def author  
+  	unless user == nil
+  	self.review.user.firstname + " " + self.review.user.lastname 
+    else
+  	"anonymous"
+  	end
   end
 
 end
