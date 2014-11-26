77ead88fafa58fbc25638814cd149a03754d60ca
zofiabialla <zofiabialla@gmail.com>
2014-11-26 19:54:46 +0100

    product fields validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+  validates_presence_of :price, :title, :description
+  validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => true
 end
