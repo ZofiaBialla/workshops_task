fdf00d8445064c7c7ebe5babc9bc0bbed8dc6dcd
zofiabialla <zofiabialla@gmail.com>
2014-11-29 12:55:53 +0100

    modify devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :firstname << :lastname
+    devise_parameter_sanitizer.for(:account_update) << :firstname << :lastname
+  end
 
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
