07375d5707141a190645a5fbc41876f73dad6fc1
zofiabialla <zofiabialla@gmail.com>
2014-11-30 12:06:37 +0100

    create users (seeds)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create([{email: 'jan@mail.com', firstname: 'Jan', lastname: 'Kowalski', password: 'NaNaNaNa', password_confirmation: 'NaNaNaNa', admin:true}, {email: 'anna@mail.com', firstname: 'Anna', lastname: 'Kowalska', password: 'NaNaNaNa', password_confirmation: 'NaNaNaNa'},
+ {email: 'marek@mail.com', firstname: 'Marek', lastname: 'Kowalski', password: 'NaNaNaNa', password_confirmation: 'NaNaNaNa'}, {email: 'kaja@mail.com', firstname: 'Kaja', lastname: 'Kowalska', password: 'NaNaNaNa', password_confirmation: 'NaNaNaNa'},
+ {email: 'adam@mail.com', firstname: 'Adam', lastname: 'Kowalski', password: 'NaNaNaNa', password_confirmation: 'NaNaNaNa'}])
