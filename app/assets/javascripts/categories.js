67d03ada5b263b1e7edb8cb655069644d8583bb9
zofiabialla <zofiabialla@gmail.com>
2014-12-01 12:35:04 +0100

    clickable thumbnails with JS highlight

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function(){
+
+  $('.thumbnail').hover(
+    function(){
+        $(this).addClass('active');
+    
+    },
+    function(){
+        $(this).removeClass('active'); 
+    }
+  );
+
+});
